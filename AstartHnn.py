a4d7eb12bef2c18bd4d152a4d0721ebee5295694
anthie951357 <54394734+anthie951357@users.noreply.github.com>
2020-06-28 21:33:23 +0800

    Update AstartHnn.py

--- AstartHnn.py
+++ AstartHnn.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import copy
 import time
@@ -91,8 +90,7 @@ def states_save(filename, states_list):
     pk.dump(states_list, f_out, -1)
     f_out.close()
 
-    import winsound
-    winsound.Beep(300, 1500)     # 其中300表示声音大小，1500表示发生时长，1000为1秒
+
 
 
 def states_save_auto(states_list):
@@ -122,12 +120,13 @@ class State(object):
         random_method = np.random.RandomState(int(time.time()))
         random_method.shuffle(self.nodes_1d[0:self.NN-1])
 
-        #self.nodes_1d = np.array([0,1,2,3,4,6,7,11,8,5,13,10,12,9,14,15], dtype='int16')
+        # self.nodes_1d = np.array([0,1,2,3,4,6,7,11,8,5,13,10,12,9,14,15], dtype='int16')
         #self.nodes_1d = np.array([1,5,3,7,0, 6,2,11,4,13,9,14,8,12,10,15], dtype='int16')
-        self.nodes_1d = np.array([0,3,8,6,14,5,9,7,13,4,11,10,12,2,1,15], dtype='int16')
-        #self.nodes_1d = np.array([9,4,0,6,2,8,3,13,5,1,11,10,12,7,14,15], dtype='int16')
+        #self.nodes_1d = np.array([0,3,8,6,14,5,9,7,13,4,11,10,12,2,1,15], dtype='int16')
+        # self.nodes_1d = np.array([9,4,0,6,2,8,3,13,5,1,11,10,12,7,14,15], dtype='int16')
         #self.nodes_1d = np.array([4,3,7,6,1,14,5,10,12,11,9,2,8,0,13,15], dtype='int16')
         #self.nodes_1d = np.array([6,12,4,7,10,3,1,2,5,9,14,13,0,8,11,15], dtype='int16')
+        self.nodes_1d = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,15,13,14], dtype='int16')
 
         while self.is_no_solution():
             random_method = np.random.RandomState(int(time.time()))
@@ -264,7 +263,10 @@ if __name__ == '__main__':
                     data = (t.nodes.flatten())
                     path_list.append(data)
                     print(t.nodes)
-
+                    x=0
+                    x+=1
+                num=len(path_list)
+                print('steps:',num)
                 #states_save_auto(path_list)
 
                 break
